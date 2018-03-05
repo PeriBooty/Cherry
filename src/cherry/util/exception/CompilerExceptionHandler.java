6bdfc94230030f297456b41ba39f70d02301f5cf
Shadow <sendmecrap@outlook.com>
2018-03-05 16:50:38 -0500

    Added MIT License and removed a few debug variables and parameters

--- src/cherry/util/exception/CompilerExceptionHandler.java
+++ src/cherry/util/exception/CompilerExceptionHandler.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 SoraKatadzuma.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package cherry.util.exception;
 
 import cherry.frontend.grammar.Token;
@@ -5,16 +28,15 @@ import java.util.LinkedList;
 import java.util.List;
 import javafx.util.Pair;
 
+// TODO: Comment methods and variables ig
 /**
  * Class for any compile errors
  * 
  * @author PeriBooty
  * @version 0.0.0.1
- * @param <T> CallingClass
  */
-public class CompilerExceptionHandler<T> {
+public class CompilerExceptionHandler {
     private final List<Pair<Throwable, ExceptionLevel>> exceptions = new LinkedList<>();
-    private final Class CallingClass;
     private final ExceptionLevel minLevel;
     
     public static enum ExceptionLevel {
@@ -26,21 +48,17 @@ public class CompilerExceptionHandler<T> {
     /**
      * Constructor 
      * 
-     * @param t The calling class instance
      * @param level The minimum exception level
      */
-    public CompilerExceptionHandler(Class t, ExceptionLevel level) {
-        CallingClass = t;
+    public CompilerExceptionHandler(ExceptionLevel level) {
         minLevel = level;
     }
     
     /**
      * Constructor
-     * @see #CompilerExceptionHandler(java.lang.Object, cherry.util.exception.CompilerExceptionHandler.ExceptionLevel)
-     * @param t The calling class instance
+     * @see #CompilerExceptionHandler(cherry.util.exception.CompilerExceptionHandler.ExceptionLevel)
      */
-    public CompilerExceptionHandler(Class t) {
-       CallingClass = t;
+    public CompilerExceptionHandler() {
        minLevel = ExceptionLevel.WARN;
     }
     
@@ -85,8 +103,8 @@ public class CompilerExceptionHandler<T> {
      * @param level The minimum level to print.
      */
     public void report(ExceptionLevel level) {
-        System.out.println("\nExceptions under the " + CallingClass.getName() + " class:");
-        exceptions.stream().filter((exception) -> (exception.getValue().ordinal() >= level.ordinal())).map((exception) -> String.format("[%s:%s] %s", CallingClass.getName(), exception.getValue().name(), exception.getKey().getMessage())).forEachOrdered((output) -> {
+        //System.out.println("\nExceptions under the " + CallingClass.getName() + " class:"); || You might wanna like, uncomment this line if you have something in mind
+        exceptions.stream().filter((exception) -> (exception.getValue().ordinal() >= level.ordinal())).map((exception) -> String.format("[%s] %s", exception.getValue().name(), exception.getKey().getMessage())).forEachOrdered((output) -> {
             System.out.println(output);
         });
         System.out.println(); // To seperate the messages
